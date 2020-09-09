8335091c2d5585b90035e9dcdf57a07ec1c5eda2
frojoe12 <joefrostdesign@gmail.com>
2020-09-09 15:31:23 +0100

    sign up/log in form addition

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
 
-const Login = () => <div></div>
+  const { email, password } = formData
+  const onFormChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  const onSubmitForm = async e => {
+    e.preventDefault()
+    console.log('success')
+  }
+  return (
+    <>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmitForm}>
+        <div className="form-group"></div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onFormChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onFormChange}
+            minLength="6"
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </form>
+    </>
+  )
+}
 
 export default Login
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Register = () => <div></div>
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { name, email, password, password2 } = formData
+  const onFormChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  const onSubmitForm = async e => {
+    e.preventDefault()
+    if (password !== password2) {
+      console.log('Passwords do not match')
+      return
+    } else {
+      console.log('success')
+    }
+  }
+  return (
+    <>
+      <h1 className="large text-primary">Register</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={onSubmitForm}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onFormChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onFormChange}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onFormChange}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={onFormChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Log In</Link>
+      </p>
+    </>
+  )
+}
 
 export default Register
